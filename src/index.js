ff0ea94afb761f8eeb8d1551dedcd1122bec5d83
j0m02be <joel.mascarenhas@walmart.com>
2019-10-24 10:08:05 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    //JS function, not specific to React
+    state = { lat: null, errorMessage: '' };
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log('Component was rendered to screen');
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('Component was updated')
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        else if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}/>
+            //return <div>Lat: {this.state.lat}</div>
+        }
+        else {
+            return <Spinner message="Please accept location request ..."/>
+            //return <div>Loading!</div>
+        }
+    }
+
+    //React requires render method otherwise it will throw error
+    render(){
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+
+    }
+}
+
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
